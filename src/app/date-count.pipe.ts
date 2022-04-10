a72e12891f2d4b0587bf5d2c7def6b17bfeb5fd7
vionaosiako <viona.osiako@student.morigaschool.com>
2022-04-10 23:36:38 +0300

    Upade pipe dateCount

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -12,12 +12,7 @@ export class DateCountPipe implements PipeTransform {
     const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
     var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
     var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-    if (dateCounter >= 1 && value > todayWithNoTime){
-      return dateCounter;
-    }else{
-      return 0;
-    }
-
+    return dateCounter;
+    
   }
 }
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,9 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    new Quote(1, 'Viona Osiako', '“As long as poverty, injustice, and gross inequality persist in our world, none of us can truly rest.” ', 'Nelson Mandela',new Date(2019,0,18),0,0),
-    new Quote(2, 'Viona Osiako', '“Human rights are not things that are put on the table for people to enjoy. These are things you fight for and then you protect.”', 'Wangari Mathai',new Date(2019,0,18),0,0),
-    new Quote(3, 'Viona Osiako', '“Change will not come if we wait for some other person, or if we wait for some other time. We are the ones we have been waiting for. We are the change that we seek.”', 'Barack Obama',new Date(2019,0,18),0,0),
+    new Quote(1, 'Viona Osiako', '“As long as poverty, injustice, and gross inequality persist in our world, none of us can truly rest.” ', 'Nelson Mandela',new Date(2019,1,18),0,0),
+    new Quote(2, 'Viona Osiako', '“Human rights are not things that are put on the table for people to enjoy. These are things you fight for and then you protect.”', 'Wangari Mathai',new Date(2020,1,18),0,0),
+    new Quote(3, 'Viona Osiako', '“Change will not come if we wait for some other person, or if we wait for some other time. We are the ones we have been waiting for. We are the change that we seek.”', 'Barack Obama',new Date(2022,1,18),0,0),
     
   ]
 
@@ -22,8 +22,23 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
-  arr: number[] = this.quotes.map(quote=>quote.upvote)
-  highest = Math.max(...this.arr)
+  // arr: number[] = this.quotes.map(quote=>quote.upvote)
+  // highest = Math.max(...this.arr)
+
+  first!:number
+  last!:number
+  count!:number
+
+  highest(){
+    this.first =0
+    this.last =0
+
+    for(this.count=0; this.count < this.quotes.length; this.count++){
+      this.last = this.quotes[this.count].upvote;
+      if(this.last > this.first){ this.first=this.last}
+    }
+    return this.first;
+  }
 
   addNewQuote(quote:Quote){
     let quoteLength = this.quotes.length;
